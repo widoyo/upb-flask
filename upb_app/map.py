cc524bf595535dbbd3e1aaa58fc38919d003ff77
Ranovan7 <romyranovan@gmail.com>
2020-04-09 13:23:31 +0700

    added details on map popups

--- upb_app/map.py
+++ upb_app/map.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from upb_app.models import Bendungan, Embung
+from upb_app.models import Bendungan, Embung, wil_sungai
 
 bp = Blueprint('map', __name__)
 
@@ -8,11 +8,32 @@ bp = Blueprint('map', __name__)
 def bendungan():
     ''' Map Bendungan '''
     bendungan = Bendungan.query.all()
-    return render_template('map/bendungan.html', bendungan=bendungan)
+    data = []
+    for b in bendungan:
+        arr = b.nama.split('_')
+        name = f"{arr[0].title()}.{arr[1].title()}"
+        data.append({
+            'id': b.id,
+            'nama': name,
+            'kab': b.kab,
+            'll': b.ll,
+            'wil': wil_sungai[b.wil_sungai],
+            'vol': "{:,.0f}".format(b.volume),
+            'lbi': "{:,.0f}".format(b.lbi),
+            'bend': b
+        })
+    return render_template('map/bendungan.html', bendungan=data)
 
 
 @bp.route('/embung')
 def embung():
     ''' Map Embung '''
     embung = Embung.query.filter(Embung.is_verified == '1').all()
-    return render_template('map/embung.html', embung=embung)
+    data = []
+    for e in embung:
+        data.append({
+            'vol': "{:,.0f}".format(e.tampungan) if e.tampungan else "-",
+            'lbi': "{:,.2f}".format(e.irigasi) if e.irigasi else "-",
+            'emb': e
+        })
+    return render_template('map/embung.html', embung=data)
